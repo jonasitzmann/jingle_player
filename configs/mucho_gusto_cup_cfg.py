14ced10d7e42802bb5639a184ab87f663f54cf2b
Jonas Sitzmann <jonasitzmann@hotmail.de>
2024-06-21 22:34:14 +0200

    mgc 2024

--- configs/mucho_gusto_cup_cfg.py
+++ configs/mucho_gusto_cup_cfg.py
@@ -10,11 +10,12 @@ from src.config import (
 mocking = (
     MockingConfig.mock_nothing()
 )  # debugging options (e.g. simulate time, don't play sound, don't access spotify)
-# mocking.begin_before_1st_job = timedelta(minutes=5)
+mocking.begin_before_1st_job = timedelta(seconds=10)
+# mocking.simulate_waiting = True
 # mocking = mocking.mock_everything()
 
 calendar = CalendarConfig(
-    calendar_url="https://calendar.google.com/calendar/ical/56b5bd48fab0022a1a8a5937a8e00f29bdb68145551b1d7bf9646b9e24c03202%40group.calendar.google.com/public/basic.ics",
+    calendar_url="https://calendar.google.com/calendar/ical/eaac02f97eb073be209b33f0256f0eae6ab70c7d7989ea3f02b452cc91005f59%40group.calendar.google.com/public/basic.ics",
     calendar_file="mucho_gusto_cup.ics",
     download_calendar=True,
 )
@@ -22,10 +23,10 @@ calendar = CalendarConfig(
 jingles = JingleConfig(
     jingles=[
         # Jingle("pre_game", offset="-3m", soundfile="pre_game.mp3"),
-        # Jingle("start", soundfile="ok_lets_go_remix.mp3", time_on_end=True),
-        Jingle("start", soundfile="ok_lets_go_remix.mp3", time_on_end=False),
-        Jingle("pre_end", offset="-5m", anchor="end"),
-        Jingle("end", anchor="end"),
+        Jingle("Start", soundfile="Start_Jingle.mp3", time_on_end=True),
+        # Jingle("start", soundfile="ok_lets_go_remix.mp3", time_on_end=False),
+        Jingle("5 min left", soundfile="5Minuten.mp3", offset="-5m", anchor="end"),
+        Jingle("End", soundfile="Outro_Jingle.mp3", anchor="end"),
     ]
 )
 
