091fc6da1dd1615a8d527cd76d44cc4e3624e594
Jonas Sitzmann <jonasitzmann@hotmail.de>
2025-08-08 15:27:54 +0200

    before fup 25

--- configs/fackel_up_cfg.py
+++ configs/fackel_up_cfg.py
@@ -15,16 +15,16 @@ mocking = (
 # mocking = mocking.mock_everything()
 
 calendar = CalendarConfig(
-    calendar_url="https://calendar.google.com/calendar/ical/a8a96c310f25dc62b169b1de208393d93f8be47bedbeaff94d21f5fc9e3a5414%40group.calendar.google.com/public/basic.ics",
+    calendar_url="https://calendar.google.com/calendar/ical/7541b5a95e60b93a9a619c028b86cc265688d859ef9e30979bfbdd7d57ffd5c9%40group.calendar.google.com/private-b0728355558f9928374e947875f8623e/basic.ics",
     calendar_file="fackelup.ics",
     download_calendar=True,
 )
 
 jingles = JingleConfig(
     jingles=[
-        Jingle("Start", soundfile="fup24/start.mp3", offset="-30s"),
-        Jingle("5 min left", soundfile="fup24/5min.mp3", offset="-5m", anchor="end"),
-        Jingle("End", soundfile="fup24/end.mp3", anchor="end"),
+        Jingle("Start", soundfile="fup25/start.mp3"),
+        Jingle("5 min left", soundfile="fup25/5_min.mp3", offset="-5m", anchor="end"),
+        Jingle("End", soundfile="fup25/end.mp3", anchor="end"),
     ]
 )
 
